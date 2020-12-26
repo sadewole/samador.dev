a985d41e6db42965022c40c128497467a6b96630
sadewole <samueladewole15@gmail.com>
2020-12-26 17:30:35 +0100

    added branch on .yml

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,38 +35,37 @@ module.exports = {
         icon: `static/images/photo2.jpg`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-remove-serviceworker`,
     // {
     //   resolve: "gatsby-plugin-remove-serviceworker",
-      //   options: {
-      //     workboxConfig: {
-      //       importWorkboxFrom: `cdn`,
-      //       runtimeCaching: [
-      //         {
-      //           // Use cacheFirst since these don't need to be revalidated (same RegExp
-      //           // and same reason as above)
-      //           urlPattern: /(\.js$|\.css$|[^:]static\/)/,
-      //           handler: "CacheFirst",
-      //         },
-      //         {
-      //           // page-data.json files, static query results and app-data.json
-      //           // are not content hashed
-      //           urlPattern: /^https?:.*\/page-data\/.*\.json/,
-      //           handler: "StaleWhileRevalidate",
-      //         },
-      //         {
-      //           // Add runtime caching of various other page resources
-      //           urlPattern: /^https?:.*\.(png|jpg|jpeg|webp|svg|gif|tiff|js|woff|woff2|json|css)$/,
-      //           handler: "StaleWhileRevalidate",
-      //         },
-      //         {
-      //           // Google Fonts CSS (doesn't end in .css so we need to specify it)
-      //           urlPattern: /^https?:\/\/fonts\.googleapis\.com\/css/,
-      //           handler: "StaleWhileRevalidate",
-      //         },
-      //       ],
-      //     },
-      //   },
+    //   options: {
+    //     workboxConfig: {
+    //       importWorkboxFrom: `cdn`,
+    //       runtimeCaching: [
+    //         {
+    //           // Use cacheFirst since these don't need to be revalidated (same RegExp
+    //           // and same reason as above)
+    //           urlPattern: /(\.js$|\.css$|[^:]static\/)/,
+    //           handler: "CacheFirst",
+    //         },
+    //         {
+    //           // page-data.json files, static query results and app-data.json
+    //           // are not content hashed
+    //           urlPattern: /^https?:.*\/page-data\/.*\.json/,
+    //           handler: "StaleWhileRevalidate",
+    //         },
+    //         {
+    //           // Add runtime caching of various other page resources
+    //           urlPattern: /^https?:.*\.(png|jpg|jpeg|webp|svg|gif|tiff|js|woff|woff2|json|css)$/,
+    //           handler: "StaleWhileRevalidate",
+    //         },
+    //         {
+    //           // Google Fonts CSS (doesn't end in .css so we need to specify it)
+    //           urlPattern: /^https?:\/\/fonts\.googleapis\.com\/css/,
+    //           handler: "StaleWhileRevalidate",
+    //         },
+    //       ],
+    //     },
+    //   },
     // },
   ],
 }
